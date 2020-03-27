44118cea9567144f69acaeaf5dec3a1adfa16319
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-03-26 20:41:04 -0500

    Basic file structure

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+//const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`Server now listening on PORT ${PORT}`);
+});
