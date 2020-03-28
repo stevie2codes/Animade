2de7666dd2e53de71cc3acbf6f0b43c2605388cf
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-03-28 10:57:39 -0500

    Modeled out the admin animation and exported it

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ if (process.env.NODE_ENV === "production") {
 
 //app.use(routes);
 
-// Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-// Start the API server
+
 app.listen(PORT, function() {
   console.log(`Server now listening on PORT ${PORT}`);
 });
