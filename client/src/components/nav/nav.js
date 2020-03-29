0639709597b0dd6ae619a1fcb6c8394af2886ff1
Stephen Webb <stephen01webb@gmail.com>
2020-03-29 10:22:07 -0500

    Initial styles
    Setup initial styles of fonts colors and nav bar

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-function Nav() {
+function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark ">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <div style={{ height: "30vmin" }} overflow="hidden">
+      <svg
+        preserveAspectRatio="none"
+        viewBox="0 0 500 150"
+        style={{ height: "100%", width: "100%" }}
+      >
+        <path
+          fill="#0B0C0F"
+          d="M-1.41 132.72c112.3-242.75 359.48 193.42 501.68-70.06L500 0H0z"
+        ></path>
+      </svg>
+    </div>
   );
 }
 
-export default Nav;
+export default NavBar;
