2929c0470ecd56c901736710d7f8709db18981cc
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-03-30 10:13:57 -0500

    finished some more cleaning up, and set up get route for admin api

--- routes/adminAPIRoute.js
+++ routes/adminAPIRoute.js
@@ -2,8 +2,7 @@ const express = require("express");
 const admin_router = express.Router();
 const db = require("../models");
 
-//set up post routes here to be used by postman, to post our admin created
-//animations
+
 admin_router.post("/api/adminAnimations", (req, res) => {
     console.log(`This is the request body ${req.body}`);
     db.admin_Animation.create(req.body).then(data => {
@@ -14,7 +13,10 @@ admin_router.post("/api/adminAnimations", (req, res) => {
 });
 
 admin_router.get("/api/adminAnimations", (req, res) => {
-    db.admin_Animation.
+    db.admin_Animation.find({}).then(data => {
+        res.json(data);
+    })
+    .catch(err => console.log(err));
 });
 
 
