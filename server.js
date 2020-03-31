b9d7555417bec828977aaec284f421926589679b
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-03-31 11:46:21 -0500

    once again, user model working, user route working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const admin_router = require("./routes/adminAPIRoute");
+const user_router = require("./routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(admin_router);
+app.use(user_router);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/animade_db");
 
