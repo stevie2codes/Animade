65d6e71c9b93fd05319e58fd22291c17d7a0c7e4
Stephen Webb <stephen01webb@gmail.com>
2020-03-31 23:28:17 -0500

    Animation page styles
    installed a list component and added animation setup

--- client/src/components/animationContainer.js
+++ client/src/components/animationContainer.js
@@ -1,24 +1,18 @@
 import React from "react";
 import "../sass/animationContainer.scss";
+import logo from "../logo.svg";
 
 const AnimationContainer = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md zoom">Zoom</div>
-        <div className="col-md">One of three columns</div>
-        <div className="col-md">One of three columns</div>
-      </div>
-      <div className="row">
-        <div className="col-md">One of three columns</div>
-        <div className="col-md">One of three columns</div>
-        <div className="col-md">One of three columns</div>
-      </div>
-      <div className="row">
-        <div className="col-md">One of three columns</div>
-        <div className="col-md">One of three columns</div>
-        <div className="col-md">One of three columns</div>
-      </div>
+    <div className="container d-flex p-5">
+      <img
+        className="row m-auto playLogo"
+        src={logo}
+        alt="logo"
+        style={{
+          borderRadius: "50%"
+        }}
+      />
     </div>
   );
 };
