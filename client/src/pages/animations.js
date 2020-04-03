c6679cbe04516e3766223b7953db38993dd95cd0
Stephen Webb <stephen01webb@gmail.com>
2020-04-02 20:41:41 -0500

    revamping animation page
    revamping animation page

--- client/src/pages/animations.js
+++ client/src/pages/animations.js
@@ -1,12 +1,30 @@
 import React from "react";
-import AnimationContainer from "../components/animationContainer";
-import Dropdown from "../components/dropdown";
+// import AnimationContainer from "../components/animationContainer";
+// import Dropdown from "../components/dropdown";
+import "../sass/animationContainer.scss";
 
 const animations = props => {
   return (
     <React.Fragment>
-      <AnimationContainer />
-      <Dropdown />
+      {/* <AnimationContainer /> */}
+      {/* <Dropdown /> */}
+      <div className="container animations">
+        <div className="row">
+          <div className="col-md zoomIn">zoomIn</div>
+          <div className="col-md">One of three columns</div>
+          <div className="col-md">One of three columns</div>
+        </div>
+        <div className="row">
+          <div className="col-md">One of three columns</div>
+          <div className="col-md">One of three columns</div>
+          <div className="col-md">One of three columns</div>
+        </div>
+        <div className="row">
+          <div className="col-md">One of three columns</div>
+          <div className="col-md">One of three columns</div>
+          <div className="col-md">One of three columns</div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
