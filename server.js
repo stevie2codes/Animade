ba3db548bb5f4012643c1b7e6ba909409550f172
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-04-03 13:42:33 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
   let url = path.join(__dirname, '../client/build', 'index.html');
   if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
-  res.sendFile(url);
+  res.sendFile(__dirname, '/index.html');
 });
 
 app.use(admin_router);
