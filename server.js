6ec5d213c4dc0f3befcbcbbd5b0a5dc326d18c08
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-04-03 13:38:04 -0500

    more deployment attempts

--- server.js
+++ server.js
@@ -13,8 +13,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //hopefully makes heroku routing work
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 app.use(admin_router);
