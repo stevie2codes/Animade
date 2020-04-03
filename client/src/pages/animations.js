24f18d4a04f63f9f3e92aa7b1ccf4b83d144c81f
Stephen Webb <stephen01webb@gmail.com>
2020-04-03 11:53:22 -0500

    adding modal
    adding modal to display code

--- client/src/pages/animations.js
+++ client/src/pages/animations.js
@@ -12,8 +12,8 @@ class animation extends Component {
     return (
       <React.Fragment>
         <AnimationContainer />
-        <p>Showing {count} animations</p>
-        <div className="container animations">
+        <p className="mt-5">Showing {count} animations</p>
+        <div className="container animations my-5">
           <div className="row">
             <div className="col-md fallAway">
               <p>Fall Away</p>
@@ -26,6 +26,39 @@ class animation extends Component {
             </div>
             <div className="col-md cartWheel">
               <p>CartWheel</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="modal" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
             </div>
           </div>
         </div>
