fd73030571289441a1c05a37d2bc7446b11a7202
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-04-03 12:13:47 -0500

    requiring path

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const admin_router = require("./routes/api/adminAPIRoute");
 const user_router = require("./routes/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
