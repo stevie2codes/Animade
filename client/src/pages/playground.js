9bcd29ad8076e0007c5da1c934e1439b05b81949
Stephen Webb <stephen01webb@gmail.com>
2020-04-05 16:53:09 -0500

    styled the pagination component
    styled the pagination component

--- client/src/pages/playground.js
+++ client/src/pages/playground.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 import "../sass/playground.scss";
+// function compile() {
+//   var html = document.getElementById("html");
+//   var css = document.getElementById("css");
+//   var js = document.getElementById("js");
+//   var code = document.getElementById("code").contentWindow.document;
 
-class Playground extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <div className="ideContainer">
-          <textarea id="html" placeholder="HTML"></textarea>
-          <textarea id="css" placeholder="CSS"></textarea>
-          <textarea id="js" placeholder="JavaScript"></textarea>
-          <iframe title="playground" id="code"></iframe>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+//   document.body.onkeyup = function() {
+//     code.open();
+//     code.writeln(
+//       html.value +
+//         "<style>" +
+//         css.value +
+//         "</style>" +
+//         "<script>" +
+//         js.value +
+//         "</script>"
+//     );
+//     code.close();
+//   };
+// }
+// compile();
+const code = document.getElementById("code").contentWindow;
+const Playground = () => {
+  return (
+    <React.Fragment>
+      <div className="ideContainer">
+        <textarea id="html" placeholder="HTML"></textarea>
+        <textarea id="css" placeholder="CSS"></textarea>
+        <textarea id="js" placeholder="JavaScript"></textarea>
+        <button onClick={() => console.log(code)}></button>
+        <iframe title="playground" id="code"></iframe>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Playground;
