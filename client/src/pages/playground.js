1ba37497db3ee6374ac35037e2d9642e6f967c0c
Stephen Webb <stephen01webb@gmail.com>
2020-04-06 00:44:07 -0500

    Functional Playground implemented
    Got the playground functioning, Having issues with the JS side but might just take that out.

--- client/src/pages/playground.js
+++ client/src/pages/playground.js
@@ -1,39 +1,108 @@
 import React, { Component } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
 import "../sass/playground.scss";
-// function compile() {
-//   var html = document.getElementById("html");
-//   var css = document.getElementById("css");
-//   var js = document.getElementById("js");
-//   var code = document.getElementById("code").contentWindow.document;
 
-//   document.body.onkeyup = function() {
-//     code.open();
-//     code.writeln(
-//       html.value +
-//         "<style>" +
-//         css.value +
-//         "</style>" +
-//         "<script>" +
-//         js.value +
-//         "</script>"
-//     );
-//     code.close();
-//   };
-// }
-// compile();
-const code = document.getElementById("code").contentWindow;
-const Playground = () => {
-  return (
-    <React.Fragment>
-      <div className="ideContainer">
-        <textarea id="html" placeholder="HTML"></textarea>
-        <textarea id="css" placeholder="CSS"></textarea>
-        <textarea id="js" placeholder="JavaScript"></textarea>
-        <button onClick={() => console.log(code)}></button>
-        <iframe title="playground" id="code"></iframe>
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+
+class Playground extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      html: "",
+      css: "",
+      js: ""
+    };
+  }
+  componentDidUpdate() {
+    this.runCode();
+  }
+
+  runCode = () => {
+    const { html, css, js } = this.state;
+
+    const iframe = this.refs.iframe;
+    const document = iframe.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Playground</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+
+        <script type="text/javascript">
+          ${js}
+        </script>
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+  render() {
+    return (
+      <div className="App">
+        <section className="playground">
+          <div className="code-editor html-code">
+            <div className="editor-header">HTML</div>
+            <CodeMirror
+              value={this.state.html}
+              options={{
+                mode: "htmlmixed"
+              }}
+              onBeforeChange={(editor, data, html) => {
+                this.setState({ html });
+              }}
+            />
+          </div>
+          <div className="code-editor css-code">
+            <div className="editor-header">CSS</div>
+            <CodeMirror
+              value={this.state.css}
+              options={{
+                mode: "css"
+                // ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, css) => {
+                this.setState({ css });
+              }}
+            />
+          </div>
+          <div className="code-editor js-code">
+            <div className="editor-header">JavaScript</div>
+            <CodeMirror
+              value={this.state.js}
+              options={{
+                mode: "javascript"
+                // ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, js) => {
+                this.setState({ js });
+              }}
+            />
+          </div>
+        </section>
+        <section className="result">
+          <iframe title="result" className="iframe" ref="iframe" />
+        </section>
       </div>
-    </React.Fragment>
-  );
-};
+    );
+  }
+}
 
 export default Playground;
