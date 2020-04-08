80b77f6c9ed52c3084647f63a53752cb8005d091
Stephen Webb <stephen01webb@gmail.com>
2020-04-08 16:02:53 -0500

    added new gradient
    added new gradient and took off svg animation

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,23 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import NavBar from "../components/nav/nav";
 
 import Cookies from "js-cookie";
 function Profile() {
-  const [activeUser] = useState(
-    Cookies.get("name")
-  );
-  
-  
-    return (
-   <div>
-        <NavBar name={activeUser}/>
-        <h1>Profile</h1>
-    </div>
-        
-       
-      
-    );
-  }
+  const [activeUser] = useState(Cookies.get("name"));
 
+  return (
+    <div>
+      <NavBar name={activeUser} />
+      <h1>Hello {activeUser}</h1>
+    </div>
+  );
+}
 
 export default Profile;
