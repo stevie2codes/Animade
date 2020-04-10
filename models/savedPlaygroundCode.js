d1bcae5113bd17fcf8544b665f2e0c48d7149efc
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-04-10 04:36:35 -0500

    model making for playground code

--- models/savedPlaygroundCode.js
+++ models/savedPlaygroundCode.js
@@ -1,34 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
+
 const savedPlaygroundCodeSchema = new Schema({
     username: {
         type: String,
         trim: true,
         required: true,
-        unique: true,
-        code: [{
-            playmation_name: {
-                type: String,
-                trim: true,
-                required: true,
-                unique: true
-            },
-            html: {
-                type: String,
-                trim: true, 
-            },
-            css: {
-                type: String,
-                trim: true
-            },
-            js: {
-                type: String,
-                trim: true,
-            }
-        }]
+    },
+    playmation_name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    html: {
+        type: String,
+        trim: true, 
+    },
+    css: {
+        type: String,
+        trim: true
+    },
+    js: {
+        type: String,
+        trim: true,
     }
-
 })
 
 const savedPlaygroundCode = mongoose.model("savedPlaygroundCode", savedPlaygroundCodeSchema);
